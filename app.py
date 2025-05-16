106131aedacbb1bac8b44f5df098e5b02598f69f
Sahar1133 <najum.bsse4171@iiu.edu.pk>
2025-05-16 21:27:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+
 
 # Machine learning and preprocessing libraries
 from sklearn.model_selection import train_test_split
